8b17f9927f9a4aee2784a06ebac997ae03148c24
irwan wahyudi <amangdoldel@gmail.com>
2025-10-24 09:22:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function cekDanArahkan() {
             window.location.href = "logaritma.html";
             break;
         case "eksponen":
-            window.location.href = "Eksponen.html";
+            window.location.href = "eksponen.html";
             break;
         case "baris dan deret":
             window.location.href = "baris dan deret.html";
@@ -22,4 +22,5 @@ function cekDanArahkan() {
             break;
     }
 }
+
 
