98809cf8e8e5a31b042485553feff504fce44689
irwan wahyudi <amangdoldel@gmail.com>
2025-10-29 20:49:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    const images = document.querySelectorAll(".slider img");
-    let currentIndex = 0;
-    const totalImages = images.length;
-    const intervalTime = 3000; // Interval waktu (dalam milidetik), contoh: 3 detik
+function cekDanArahkan() {
+    let kataInput = document.getElementById("kotakInput").value.toLowerCase();
 
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % totalImages;
-        const offset = -currentIndex * 100;
-        slider.style.transform = `translateX(${offset}%)`;
+     switch (kataInput) {
+        case "trigonometri":
+            window.location.href = "trigonometri.html";
+            break;
+	case "trigono":
+            window.location.href = "trigonometri.html";
+            break;
+	case "trig":
+            window.location.href = "trigonometri.html";
+            break;
+
+
+        case "logaritma":
+            window.location.href = "logaritma.html";
+            break;
+ 	case "log":
+            window.location.href = "logaritma.html";
+            break;
+
+
+        case "eksponen":
+            window.location.href = "eksponen.html";
+            break;
+
+        case "baris dan deret":
+            window.location.href = "baris dan deret.html";
+            break;
+ 	case "baris":
+            window.location.href = "baris dan deret.html";
+            break;
+ 	case "deret":
+            window.location.href = "baris dan deret.html";
+            break;
+ 	case "baris aritmatik":
+            window.location.href = "baris dan deret.html";
+            break;
+ 	case "deret aritmatik":
+            window.location.href = "baris dan deret.html";
+            break;
+	 case "baris geometri":
+            window.location.href = "baris dan deret.html";
+            break;
+ 	case "deret geometri":
+            window.location.href = "baris dan deret.html";
+            break;
+
+        case "limit":
+            window.location.href = "limit.html";
+             break;
+	case "limit fungsi":
+            window.location.href = "limit.html";
+             break;
+        default:
+            alert("Kata tidak ditemukan. Coba lagi!");
+            break;
     }
-
-    setInterval(nextSlide, intervalTime);
-});
+}
